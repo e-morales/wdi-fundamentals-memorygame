27794e18bd43790be7f384a27cc2f9a5f5f7ac1b
e-morales <e.morales@digitalnest.org>
2019-01-15 15:17:16 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,61 @@
-var cards = [ 'queen', 'queen', 'king', 'king'];
+var cards = [
+    {
+        rank: 'queen',
+        suit: 'hearts',
+        cardImage: 'images/queen-of-hearts.png'
+    },
+    {
+        rank: 'queen',
+        suit: 'diamonds',
+        cardImage: 'images/queen-of-diamonds.png'
+    },
+    {
+        rank: 'king',
+        suit: 'hearts',
+        cardImage: 'images/king-of-hearts.png'
+    },
+    {
+        rank: 'king',
+        suit: 'diamonds',
+        cardImage: 'images/king-of-diamonds.png'
+    }
+];
+
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+var checkForMatch = function(){
+    if (cardsInPlay[0].rank === cardsInPlay[1].rank){
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+};
+   
 
-console.log('User flipped ' + cardsInPlay[0]);
-console.log('User flipped ' + cardsInPlay[1]);
+var flipCard = function() {
+    var cardId = this.getAttribute('data-id');
+     cardsInPlay.push(cards[cardId].rank);
+     console.log("User flipped " + cards[cardId].rank);
+     console.log(cards[cardId].cardImage);
+     console.log(cards[cardId].suit);
+     this.setAttribute('src', cards[cardId].cardImage);
+     if (cardsInPlay.length === 2){
+        checkForMatch();
+    } 
+};
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-    alert('You found a match!');
-} else {
-    alert('Try again, buddy.');
+var createBoard = function(){
+    for (var i = 0; i < cards.length; i++){
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
 }
+
+var button = document.getElementById('reset');
+button.addEventListener('reset', cardsInPlay);
+
+createBoard();
+
